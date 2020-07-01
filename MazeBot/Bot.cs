8869bf35079c560e003c480e9cd3d5cefed50ab3
Вдовенко Віталій <taybenberg@gmail.com>
2020-07-01 13:03:19 +0300

    Bot template

--- MazeBot/Bot.cs
+++ MazeBot/Bot.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.IO;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.InlineQueryResults;
 
 namespace MazeBot
 {
     public class Bot
     {
+        private TelegramBotClient bot;
+
+        public void Start() => bot.StartReceiving();
+
+        public void Stop() => bot.StopReceiving();
+
+        public Bot(string TelegramApiToken)
+        {
+            bot = new TelegramBotClient(TelegramApiToken);
+
+            bot.SetWebhookAsync("");
+
+            bot.OnInlineQuery += async (object updobj, InlineQueryEventArgs iqea) =>
+            {
+
+            };
+
+            bot.OnMessage += async (object updobj, MessageEventArgs mea) =>
+            {
+
+            };
+        }
     }
 }
